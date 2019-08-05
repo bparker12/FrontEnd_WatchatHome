e0271e205df26fed1e14530357ad8990bff7db2f
bparker12 <49100894+bparker12@users.noreply.github.com>
2019-08-05 14:00:15 -0500

    started on form

--- src/Components/WatchComp/Review/Review.js
+++ src/Components/WatchComp/Review/Review.js
@@ -7,7 +7,22 @@ export default class Review extends Component {
     render() {
         return (
             <div>
-                
+                <Form>
+                    {this.props.watchlist.reviewId === null ?
+                        <React.Fragment>
+                            <div hidden={this.state.hidden}>
+                                <Button compact onClick={this.handleReviewClick}>Add a Review</Button>
+                            </div>
+                            <div hidden={!this.state.hidden}>
+                                <NewReview handleReviewClick={this.handleReviewClick} updateCard={this.props.updateCard} watchlist={this.props.watchlist} />
+                            </div>
+                        </React.Fragment>
+                        :
+                        <div>
+                            <Message content={this.props.watchlist.review} header="Review" handleReviewClick={this.handleReviewClick} updateCard={this.props.updateCard} />
+                        </div>
+                    }
+                </Form>
             </div>
         )
     }
