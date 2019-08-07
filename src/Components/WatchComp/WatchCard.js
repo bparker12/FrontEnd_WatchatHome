ddb93900de09f2c5e4a224516ec7dc3300dcc75c
bparker12 <49100894+bparker12@users.noreply.github.com>
2019-08-07 16:10:47 -0500

    add a detail page to the watch card

--- src/Components/WatchComp/WatchCard.js
+++ src/Components/WatchComp/WatchCard.js
@@ -1,11 +1,16 @@
 import React, { Component } from 'react';
-import { Card, Image, Button, Confirm, Checkbox } from 'semantic-ui-react';
+import { Card, Image, Button, Confirm, Checkbox, Dimmer, Header, Modal, Container } from 'semantic-ui-react';
 import Favorites from './Favorites/Favorites';
 import Review from './Review/Review'
+import  './WatchCard.css'
 
 export default class WatchCard extends Component {
 
-    state = { open: false };
+    state = {
+        open: false,
+    };
+    handleShow = () => this.setState({ active: true })
+    handleHide = () => this.setState({ active: false })
 
     //--For Confirmation Box--//
     open = () => this.setState({ open: true });
@@ -46,6 +51,41 @@ export default class WatchCard extends Component {
     }
 
     render() {
+        const { active } = this.state
+        const content = (
+            <div>
+                <Header as='h2' inverted>
+                    Details
+              </Header>
+                <Modal trigger={<Button>View</Button>}>
+                    <Modal.Header>{this.props.watchlist.Title}</Modal.Header>
+                    <Modal.Content image scrolling>
+                        <Image size='medium' src={this.props.watchlist.Poster} wrapped/>
+                        <Modal.Description>
+                                <Container>
+                                <Header>{this.props.watchlist.Type} overview</Header>
+                            <ul>
+                                <li><strong>Year</strong>: {this.props.watchlist.Year}</li>
+                                <li><strong>Actors</strong>: {this.props.watchlist.Actors}</li>
+                                <Container >
+                                <li><strong>Plot</strong>: {this.props.watchlist.Plot}</li>
+                                </Container>
+                                <li><strong>Director</strong>: {this.props.watchlist.Director}</li>
+                                <li><strong>Genre</strong>: {this.props.watchlist.Genre}</li>
+                                <li><strong>Rated</strong>: {this.props.watchlist.Rated}</li>
+                                <li><strong>Runtime</strong>: {this.props.watchlist.Runtime}</li>
+                                <li><strong>Awards</strong>: {this.props.watchlist.Awards}</li>
+                                <li><strong>BoxOffice</strong>: {this.props.watchlist.BoxOffice}</li>
+                                <li><strong>Production</strong>: {this.props.watchlist.Production}</li>
+                                <li><strong>Writer</strong>: {this.props.watchlist.Writer}</li>
+                                <li><strong>imdbRating</strong>: {this.props.watchlist.imdbRating}</li>
+                             </ul>
+                                </Container>
+                        </Modal.Description>
+                    </Modal.Content>
+                </Modal>
+            </div>
+        )
         // console.log("watchlist props", this.props.watchlist)
         let checkLabel = this.checkboxLabel()
         return (
@@ -53,7 +93,15 @@ export default class WatchCard extends Component {
                 <Card.Content>
                     <Favorites watchlist={this.props.watchlist} updateCard={this.props.updateCard} />
                 </Card.Content>
-                <Image src={this.props.watchlist.Poster} wrapped ui={false} />
+                <Dimmer.Dimmable
+                    as={Image}
+                    src={this.props.watchlist.Poster}
+                    wrapped ui={false}
+                    dimmed={active}
+                    dimmer={{ active, content }}
+                    onMouseEnter={this.handleShow}
+                    onMouseLeave={this.handleHide}
+                />
                 <Card.Content>
                     <Checkbox label={checkLabel} defaultChecked={this.props.watchlist.watched} onChange={this.watchedToggle} />
                     <Card.Header>{this.props.watchlist.Title}</Card.Header>
@@ -67,7 +115,7 @@ export default class WatchCard extends Component {
                     <Button onClick={this.open}>
                         Remove from Watchlist
                 </Button>
-                    <Confirm open={this.state.open}
+                    <Confirm size="mini" open={this.state.open}
                         onCancel={this.close}
                         onConfirm={() => this.props.deleteCard("watchlists", this.props.watchlist.id)}
                     />
